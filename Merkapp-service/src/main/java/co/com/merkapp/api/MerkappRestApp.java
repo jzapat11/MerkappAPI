36eaad32fe3c200c7b3192063fb5c83325b36717
Juan Zapata <Juan.Y.Zapata.-ND@disney.com>
2017-11-15 11:56:19 -0800

    Add basis to the REST controller module
    Adding the early basis for the REST controller module

---
+++ Merkapp-service/src/main/java/co/com/merkapp/api/MerkappRestApp.java
@@ -0,0 +1,14 @@
+package co.com.merkapp.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication(scanBasePackages = {"co.com.merkapp.api"})
+@ImportResource({"classpath* :application-context.xml"})
+public class MerkappRestApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerkappRestApp.class, args);
+	}
+}
---
+++ Merkapp-service/src/main/java/co/com/merkapp/api/controller/MerkappRestController.java
@@ -0,0 +1,14 @@
+package co.com.merkapp.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MerkappRestController {
+
+    public ResponseEntity<?> login(){
+        return null;
+    }
+}
